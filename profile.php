e577500ce275cf9d18363fa2c7add0725beca41c
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-01-17 08:46:41 +0200

    Profile backend

--- profile.php
+++ profile.php
@@ -1,7 +1,18 @@
 <?php
+include('classes/DB.php');
 include('includes/head.php');
 include('includes/header.php');
 
+if(!isset($_GET['id'])){
+  header('Location: ./');
+}
+$member_id = $_GET['id'];
+
+//Fetching member's data + there committee's name
+$member = DB::query('SELECT *, m.name as name, c.name as cname 
+                     FROM members m,committees c
+                     WHERE m.id=:id AND m.committee_id = c.id',
+                     array(':id'=>$member_id))[0];
 
  ?>
  <div id="main-body">
@@ -14,9 +25,11 @@ include('includes/header.php');
                <img src="layout/png/profile.png" alt="">
              </div>
            </div>
-           <h2>Mohammad Ashraf <span class="nickname">(xTrimy)</span> </h2>
+           <h2><?php echo $member['name'] ?> 
+           <!-- <span class="nickname">(xTrimy)</span> -->
+            </h2>
            <div class="tags">
-            <div class="item committee">Personnell</div>
+            <div class="item committee"><?php echo $member['cname'] ?> </div>
             <div class="item position">Co-head</div>
             <div class="item it">IT Specialist</div>
             <div class="item special">Member of the Year 2019-2020</div>
@@ -33,23 +46,23 @@ include('includes/header.php');
          <table class="data-table">
            <tr>
              <td><b>Name</b></td>
-             <td>Mohammed Ashraf</td>
+             <td><?php echo $member['name'] ?> </td>
            </tr>
            <tr>
              <td><b>ID</b></td>
-             <td>2018/12470</td>
+             <td><?php echo $member['university_id'] ?> </td>
            </tr>
            <tr>
              <td><b>Email</b></td>
-             <td>Mohamed1812470@miuegypt.edu.eg</td>
+             <td><?php echo $member['email'] ?> </td>
            </tr>
            <tr>
              <td><b>Phone</b></td>
-             <td>01156052920 <a href="#">Edit</a></td>
+             <td><?php echo $member['phone'] ?></td>
            </tr>
            <tr>
              <td><b>Committee</b></td>
-             <td>Personnell</td>
+             <td><?php echo $member['cname'] ?> </td>
            </tr>
          </table>
          <div class="xbutton secondary"><i class="fas fa-cog"></i> Edit Profile </div>
