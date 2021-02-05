683153ab68bed2f866161727de99300ab07f5f04
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-02-06 01:48:01 +0200

    Edit login page

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@800&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet">
