b4f1cf38ce599953fc8df9c29615465fafbe527b
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-02-06 20:52:22 +0200

    Extend member login cookie lifetime

--- classes/Login.php
+++ classes/Login.php
@@ -14,8 +14,8 @@ public static function isLoggedIn() {
                         $timeNow = date('Y-m-d H:i:s');
                         DB::query('INSERT INTO member_login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$userid));
                         DB::query('DELETE FROM member_login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['Campaigners_ID'])));
-                        setcookie("Campaigners_ID", $token, time() + 60 * 60 * 1, '/', NULL, NULL, TRUE); //setting cookie to 1 hour
-                        setcookie("Campaigners_ID_", '1', time() + 60 * 30, '/', NULL, NULL, TRUE); //setting (rest) cookie to 30 minutes
+                        setcookie("Campaigners_ID", $token, time() + 60 * 60 * 24 * 3, '/', NULL, NULL, TRUE); //setting cookie to 3 days
+                        setcookie("Campaigners_ID_", '1', time() + 60 * 60 * 12, '/', NULL, NULL, TRUE); //setting (rest) cookie to 12 hours
                         return $userid;
                     }
             }
