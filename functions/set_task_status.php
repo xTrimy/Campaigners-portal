f94698bbe95b4c42ef6aff71be6e331ab0ab19e0
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-02-11 08:30:59 +0200

    Add comments to explain files

--- functions/set_task_status.php
+++ functions/set_task_status.php
@@ -2,18 +2,20 @@
 include('start.php');
 header('Content-Type: application/json; charset=utf-8', true);
 $response = new stdClass();
+// Check if request sent correctly
 if(isset($_POST['id']) && isset($_POST['value'])){
     $id = $_POST['id'];
     $value = $_POST['value'];
     $value = $value === 'true'? 1: 0;
-    if(DB::query('SELECT 1 FROM tasks WHERE id=:id',array(':id'=>$id))){
-        if(!DB::query('SELECT 1 FROM tasks WHERE id=:id AND member_id=:member_id',array(':id'=>$id,':member_id'=>$user_id))){
+    if(DB::query('SELECT 1 FROM tasks WHERE id=:id',array(':id'=>$id))){ // Check if task exists
+        if(!DB::query('SELECT 1 FROM tasks WHERE id=:id AND member_id=:member_id',array(':id'=>$id,':member_id'=>$user_id))){ // Check if user not allowed to send this request
             http_response_code(403);
             $response->status = "Forbidden";
             $response->code = "403";
             $response->time = date('Y-m-d H:i:s');
             $response->error = "Forbidden";
     }
+        // Update the task
         DB::query('UPDATE tasks SET is_finished=:is_finished WHERE id=:id',array(':id'=>$id,':is_finished'=>$value));
         http_response_code(200);
         $response->message = "Task status updated";
@@ -21,7 +23,7 @@ if(isset($_POST['id']) && isset($_POST['value'])){
         $response->status = "Ok";
         $response->code = "200";
         $response->time = date('Y-m-d H:i:s');
-    }else{
+    }else{ //Task not found
         http_response_code(404);
         $response->message = "Task not found";
         $response->status = "Not found";
@@ -29,12 +31,13 @@ if(isset($_POST['id']) && isset($_POST['value'])){
         $response->time = date('Y-m-d H:i:s');
         $response->error = "Not found";
 }
-}else{
+}else{ // Request isn't correct (parameters not correct {$id},{$value})
     http_response_code(400);
     $response->status = "Bad request";
     $response->code = "400";
     $response->time = date('Y-m-d H:i:s');
     $response->error = "Bad request";
 }
+//Send JSON encoded response
 echo json_encode($response);
 ?>
--- layout/js/alert-box.js
+++ layout/js/alert-box.js
@@ -1,6 +1,8 @@
 //!Do NOT Edit on this file
 // Custom alert box START
 var alert_container = document.getElementById('alert-boxes-container');
+
+// Create an instance of the alert box HTML structure (alert_box)
 var alert_box = document.createElement('div'),
     alert_icon = document.createElement('div'),
     alert_content = document.createElement('div'),
@@ -13,17 +15,32 @@ alert_box.appendChild(alert_icon);
 alert_box.appendChild(alert_content);
 alert_box.appendChild(alert_close);
 
+// Function to display the alert box
 function box_alert(type,message,duration = 5000){
+  if(duration<1000){
+    duration = 1000;
+  }
+  duration+=400;
+  // Clone an instance of the alert_box we made above (alert_box)
   let this_alert = alert_box.cloneNode(true);
+  // add the type of the alert_box (can be success, warning, etc..)
   this_alert.classList.add(type);
+  // add the alert box to the page body
   alert_container.appendChild(this_alert);
+  // add the message content to the alert box
   this_alert.querySelector('.content').innerHTML = message;
+  // by default, the alert box isn't visible. We use the bellow code to show it with an animation
   setTimeout(function(){
     this_alert.classList.add('show');
   },100);
+
+  // Hide the alert_box after being displayed for amount of time (duration)
   setTimeout(function(){
+    //check if user already hide the alert_box
   if(document.body.contains(this_alert)){
+    // animate the alert_box to be hidden
   this_alert.classList.remove('show');
+    // remove the alert_box from the DOM after the animation
   setTimeout(function(){
     if(document.body.contains(this_alert)){
       alert_container.removeChild(this_alert);
@@ -31,9 +48,10 @@ function box_alert(type,message,duration = 5000){
     },400);
   }
   },duration-400);
+  // function to handle the close button click event
   box_alert_close_button(this_alert);
 }
-
+// function to handle the close button click event
 function box_alert_close_button(el){
   let this_alert = el;
   this_alert.querySelector('.close-button').addEventListener('click',function(){
